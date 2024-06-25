478ed8c3307c49eb845c1406018a0d98bbd67899
SombatBuekeaw <Sombat073855@gmil.com>
2024-06-25 13:45:58 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ color: "red", textAlign: "center" }}>Welcome to DevOps</h1>
+      <hr />
+      <div style={{ textAlign: "center" }}>
+        <h2>My Name is: Mr.Jhon Doe</h2>
+       
+      </div>
+    </div>
+  );
+};
+
+export default App;
