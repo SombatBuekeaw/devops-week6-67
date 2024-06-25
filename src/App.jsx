4f8eda0d0f25a2d8f92a82bbd29aabbaca519c35
SombatBuekeaw <Sombat073855@gmil.com>
2024-06-25 13:57:29 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 const App = () => {
   return (
-    <div>
+    <>
       <h1 style={{ color: "red", textAlign: "center" }}>Welcome to DevOps</h1>
       <hr />
       <div style={{ textAlign: "center" }}>
-        <h2>My Name is: Mr.Jhon Doe</h2>
-       
+        <h2>My Name is: SOMBAT</h2>
+        <img
+          src="https://cdn.pixabay.com/photo/2024/02/04/04/11/fashion-8551487_640.jpg"
+          width={350}
+          height={250}
+          alt=""
+        />
       </div>
-    </div>
+    </>
   );
 };
 
